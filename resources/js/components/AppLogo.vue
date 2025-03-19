7fc89cd85d8882754530c40b085dfb95fa436f51
Ooi Kim Siah <kimsiahooi@gmail.com>
2025-03-20 05:30:26 +0800

    ui: change title and logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,12 +1,10 @@
-<script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+        <img src="/assets/images/favicon.png" alt="logo" class="size-5" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate font-semibold leading-none">Laravel Starter Kit</span>
+        <span class="mb-0.5 truncate font-semibold leading-none">Cream Couch</span>
     </div>
 </template>
